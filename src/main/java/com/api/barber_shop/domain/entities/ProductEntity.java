93a83ec110310c2d9d18a927e12d8d7e376b72ab
Bruno Mota <contatobrunotr@gmail.com>
2025-08-09 20:04:28 -0300

    feat: adc Lombok

--- src/main/java/com/api/barber_shop/domain/entities/ProductEntity.java
+++ src/main/java/com/api/barber_shop/domain/entities/ProductEntity.java
@@ -1,29 +1,35 @@
 package com.api.barber_shop.domain.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
-@Table(name = "services")
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProductEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id_service", nullable = false)
+    @Column(name = "id_product", nullable = false)
     private UUID idService;
 
-    @Column(name = "name_service")
+    @Column(name = "name_product")
     private String nameService;
 
-    @Column(name = "duration_min_service")
+    @Column(name = "duration_min_product")
     private Integer durationMinService;
 
-    @Column(name = "price_service")
+    @Column(name = "price_product")
     private BigDecimal priceService;
 
-    @Column(name = "active_service")
+    @Column(name = "active_product")
     private Boolean activeService;
 }
 
